507761769f92e96909eef727a80d21b70002b01a
1dami <1dami@HZ301502.TartangaLH.eus>
2021-05-31 17:32:04 +0200

    Rama con javadoc

--- src/numero/Numero.java
+++ src/numero/Numero.java
@@ -1,19 +1,35 @@
 package numero;
 
 import java.util.Scanner;
+/**
+ * 
+ * @author Emil
+ *@since 31/05/2021
+ * Version 1, 2021
+ */
 
 public class Numero {
 	public static void main(String[] arg) {
+		/**
+		 * Variables y Scanner 
+		 */
 		Scanner teclado=new Scanner (System.in);
 		double n1, n2;
 		double cociente;
 		
-		System.out.println("Introduce primer n�mero entero:");
+		/**
+		 * Se introduce primer numero
+		 */
+		System.out.println("Introduce primer nmero entero:");
 		n1=teclado.nextDouble();
-		
-		System.out.println("Introduce segundo n�mero entero:");
+		/**
+		 * Se introduce el segundo numero
+		 */
+		System.out.println("Introduce segundo nmero entero:");
 		n2=teclado.nextDouble();
-		
+		/**
+		 * Se saca el cociente
+		 */
 		cociente=n1/n2;
 		
 		System.out.println(n1+" / "+n2+" = "+cociente);
